e053f6ac2d6f9dd080a0569df30fa8d5b9464800
schneiderkarel <schne1karel@gmail.com>
2023-04-24 19:28:47 +0200

    uuCB-3: ESLint

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import React from 'react';
+
 const App = () => (
-    <div className="App">
-        <button type="button" className="btn btn-outline-primary">Primary</button>
-    </div>
+  <div className="App">
+    <button type="button" className="btn btn-outline-primary">
+      Primary
+    </button>
+  </div>
 );
 
 export default App;
