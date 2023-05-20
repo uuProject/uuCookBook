0c135360e6c987e3dda3f6dbf54ad8ee562cf064
František Majer <majer.frantisek@hotmail.com>
2023-05-20 22:59:30 +0200

    uuCB-17: Add dotenv

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const port = 8080;
+const port = process.env.HTTP_SERVER_PORT;
 
 app.get('/status', (req, res) => {
   res.status(200).send('');
