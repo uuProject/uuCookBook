9014b0b123cce0f2e325553ab19ef15bb6c059fb
LLosTy <marcojurgenjuras@gmail.com>
2023-06-04 20:28:51 +0200

    uuCB-9 Added search bar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -13,12 +13,25 @@ const Navbar = () => {
   ];
 
   const [activeItemName, setActiveItemName] = useState('');
+  const [searchInput, setSearchInput] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (searchInput.length > 0) {
+      console.log(searchInput);
+    }
+  };
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    setSearchInput(event.target.value);
+  };
 
   return (
     <nav className="navbar navbar-expand navbar-light bg-light">
       <div className="container-fluid">
         <a className="navbar-brand" href="/#" onClick={() => setActiveItemName('')}>Cook Book</a>
-        <div className="collapse navbar-collapse mx-2" id="navbarSupportedContent">
+        <div className="collapse navbar-collapse mx-2d-flex justify-content-between" id="navbarSupportedContent">
           <ul className="navbar-nav">
             {items.map(({ name, link }) => (
               <li className="nav-item">
@@ -32,6 +45,10 @@ const Navbar = () => {
               </li>
             ))}
           </ul>
+          <form className="d-flex" role="search">
+            <input className="form-control me-1" type="search" placeholder="Search" aria-label="Search" onChange={handleChange} value={searchInput} />
+            <button className="btn btn-outline-primary" type="submit" onClick={handleSubmit}>Search</button>
+          </form>
         </div>
       </div>
     </nav>
