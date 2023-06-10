65438eaeaa3bf7e900957bf7e828814ae1587f09
LLosTy <marcojurgenjuras@gmail.com>
2023-06-10 10:30:48 +0200

    Redo of Card

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Card from './components/Card';
 
 const App = () => (
   <div className="App">
     <Navbar />
+    <Card />
   </div>
 );
 
